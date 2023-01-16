543968a322bbfb4a5fcddb39efc939c4f675b45a
lthoreson <loganthoreson@gmail.com>
2023-01-16 12:31:41 -0600

    fixed card/button highlight issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { PantryComponent } from './components/pantry/pantry.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
+import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { EditItemComponent } from './components/edit-item/edit-item.component';
+import { EditRecipeComponent } from './components/edit-recipe/edit-recipe.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
@@ -17,16 +25,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { PantryComponent } from './components/pantry/pantry.component';
 import {MatCardModule} from '@angular/material/card';
-import { AddItemComponent } from './components/add-item/add-item.component';
-import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
-import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
 import {MatStepperModule} from '@angular/material/stepper';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecipeComponent } from './components/recipe/recipe.component';
-import { EditItemComponent } from './components/edit-item/edit-item.component';
-import { EditRecipeComponent } from './components/edit-recipe/edit-recipe.component';
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -57,6 +58,7 @@ import { EditRecipeComponent } from './components/edit-recipe/edit-recipe.compon
     MatSnackBarModule,
     MatCardModule,
     MatStepperModule,
+    MatChipsModule,
     FormsModule,
     ReactiveFormsModule
   ],
